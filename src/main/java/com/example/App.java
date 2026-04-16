d28f6dc3ebb53314b68a6276400bccd490e7eb9c
Vachanbs <vachanbs21@gmail.com>
2026-04-16 12:03:28 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Vachan 1BI23CS232" );
+    }
+}
